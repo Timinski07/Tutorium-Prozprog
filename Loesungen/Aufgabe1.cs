8c4b35acd1536fdc94592ee29975acd40a67cc58
Timinski07 <92364903+Timinski07@users.noreply.github.com>
2022-11-10 12:02:24 +0100

    Create Aufgabe1.cs

---
+++ Loesungen/Aufgabe1.cs
@@ -0,0 +1,28 @@
+//Aufgabe 1
+//a)
+Console.Write("Geben Sie die Anzahl der Studenten im Raum ein: ");
+int anzahl = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= anzahl; i++)
+{
+    Console.WriteLine($"Rechner Nr.{i} wird hochgefahren");
+    if (i % 15 == 0)
+    {
+        Console.WriteLine("Neue CPU hinzugefügt!");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Rechner herunterfahren?(Press Key)");
+Console.ReadLine();
+//b)
+while (anzahl > 0)
+{
+    if (anzahl % 8 == 0) {
+        Console.WriteLine($"Rechner Nr.{anzahl}: Alle Rechner werden heruntergefahren");
+        anzahl = 0;
+    }
+    else
+    {
+        Console.WriteLine($"Rechner Nr.{anzahl} wird heruntergefahren");
+        --anzahl;
+    }
+}
